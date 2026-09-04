f00dc3332dba133eb961b95b537b8a2c2a568a98
Jaren Haro <110628142+JarenHaro@users.noreply.github.com>
2026-09-04 00:57:14 -0700

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows = {}
+        cols = {}
+        for r in range(9):
+            rows[r] = set()
+        for c in range(9):
+            cols[c] = set()
+        for r in range(9):
+            for c in range(9):
+                
+                row_value = board[r][c]
+                col_value = board[c][r]
+                if row_value in rows[r] and row_value != '.':
+                    return False
+                rows[r].add(row_value)
+                if col_value in cols[r] and col_value != '.':
+                    return False
+                cols[r].add(col_value)
+
+        box_of_threes = {}
+
+        for b_row in range(3):
+            for b_col in range(3):
+                box_of_threes[(b_row, b_col)] = set()
+        
+        for r in range(9):
+            for c in range(9):
+                b_r = r // 3
+                b_c = c // 3
+                value = board[r][c]
+                if value in box_of_threes[b_r, b_c] and value != '.':
+                    return False
+                box_of_threes[b_r, b_c].add(value)
+        return True
+            
+
+        
+        
+        
+
+
